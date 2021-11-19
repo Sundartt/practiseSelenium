a1f506a57bcbb44fa3cba547908226ce71c92232
Sundartt <94391316+Sundartt@users.noreply.github.com>
2021-11-19 15:33:40 +0530

    Update Button.java

--- nov18/Button.java
+++ nov18/Button.java
@@ -14,12 +14,24 @@ public class Button {
         ChromeDriver driver=new ChromeDriver();
         driver.get("http://leafground.com/pages/Button.html");
         driver.manage().window().maximize();
-       // driver.findElement(By.xpath("//button[text()='Go to Home Page']")).click();
+		driver.findElement(By.xpath("//button[text()='Go to Home Page']")).click();
+        String title = driver.getTitle();
+        System.out.println(title);
+        if(title.contains("Selenium"))
+        {
+        	System.out.println("Yes");
+        }
+        else
+        {
+        	System.out.println("Noo");
+        }
+        driver.navigate().to("http://leafground.com/pages/Button.html");
+        
           //position
         Point location = driver.findElement(By.xpath("//button[text()='Get Position']")).getLocation();
         System.out.println(location);
           //Button color
-        String cssValue = driver.findElement(By.xpath("//button[text()='What color am I?']")).getCssValue("style");
+        String cssValue = driver.findElement(By.xpath("//button[text()='What color am I?']")).getCssValue("color");
         System.out.println(cssValue);
           //Height
         int cssValue2 = driver.findElement(By.xpath("//button[text()='What is my size?']")).getSize().getHeight();
